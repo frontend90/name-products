b2d33bba13d53a355f803623c88e85abccb47b3e
frontend90 <leon44173@gmail.com>
2020-03-20 10:28:49 +0200

    верстка

--- js/main.js
+++ js/main.js
@@ -5,3 +5,21 @@ wow = new WOW({
   offset: 0 // default
 })
 wow.init();
+
+/*********************  плавный скролл    *********/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
+  });
+});
